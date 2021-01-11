046949fc65c5940811f3b61edd6497b1a5216d08
GuinhoHenriq <mguinhoguido@gmail.com>
2021-01-11 17:53:44 -0300

    API terminada

--- AKIVA_API/Controllers/EnviaInfoController.cs
+++ AKIVA_API/Controllers/EnviaInfoController.cs
@@ -6,6 +6,9 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Data.SqlClient;
 using System.Data;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace AKIVA_API.Controllers
 {
@@ -23,13 +26,15 @@ namespace AKIVA_API.Controllers
             try
             {
                 ds = dados.CarregaCartao(objCartao);
-
+                
                 objCartao.Cliente_Clicodigo = Convert.ToInt32(ds.Tables[0].Rows[0]["CLICODIGO"].ToString());
                 objCartao.Cliente_NumCartao = ds.Tables[0].Rows[0]["NUMERO_CARTAO"].ToString();
                 objCartao.Cliente_DtValidadeCartao = ds.Tables[0].Rows[0]["DATA_VALIDADE"].ToString();
                 objCartao.Cliente_CVVCartao = ds.Tables[0].Rows[0]["CVV"].ToString();
                 objCartao.Num_Retorno = ds.Tables[0].Rows[0]["RETORNO"].ToString();
 
+                objCartao.Cliente_NumCartao = (Decrypt(objCartao.Cliente_NumCartao));
+                objCartao.Cliente_CVVCartao = (Decrypt(objCartao.Cliente_CVVCartao));
             }
             catch (Exception ex)
             {
@@ -38,10 +43,32 @@ namespace AKIVA_API.Controllers
             finally
             { };
 
-            return Request.CreateResponse(HttpStatusCode.OK, ds);
+            string textjson = objCartao.Cliente_Clicodigo + "|" + objCartao.Cliente_NumCartao + "|" + objCartao.Cliente_DtValidadeCartao + "|" + objCartao.Cliente_CVVCartao + "|" + objCartao.Num_Retorno;
+            
+
+            return Request.CreateResponse(HttpStatusCode.OK, textjson);
         }
 
+        #endregion
 
+        #region Método que Decriptografa as informações do BD
+        static string Decrypt(string Code)
+        {
+            Chilkat.Crypt2 crypt = new Chilkat.Crypt2();
+            crypt.CryptAlgorithm = "AES";
+            crypt.CipherMode = "ECB";
+            crypt.KeyLength = 128;
+            crypt.PaddingScheme = 0;
+            crypt.EncodingMode = "HEX";
+
+            //string keyHex = "4D43454E56444556";
+            string keyHex = "34443433343534453536343434353536";
+            crypt.SetEncodedKey(keyHex, "hex");
+
+            string decStr = crypt.DecryptStringENC(Code);
+
+            return decStr;
+        }
         #endregion
     }
 }
